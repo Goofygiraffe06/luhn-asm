81c1d2a643ab9f0c64e088b5a4ad669520e48469
Goofygiraffe06 <rahul.narsing06@gmail.com>
2025-03-15 17:43:46 +0530

    optimize: implemented jump table for card detection

--- cardtype.asm
+++ cardtype.asm
@@ -5,6 +5,15 @@ amex_msg    db      'Card type: American Express', 0Ah, 0h  ; Amex message
 disc_msg    db      'Card type: Discover', 0Ah, 0h       ; Discover message
 unk_msg     db      'Card type: Unknown', 0Ah, 0h        ; Unknown type message
 
+jmp_tbl:
+        dd      detect_card_type.unknown
+        dd      detect_card_type.unknown
+        dd      detect_card_type.unknown
+        dd      detect_card_type.check_amex
+        dd      detect_card_type.visa
+        dd      detect_card_type.check_mastercard
+        dd      detect_card_type.check_discover
+
 SECTION     .text
 
 detect_card_type:                   ; Card type detection module
@@ -14,15 +23,9 @@ detect_card_type:                   ; Card type detection module
         mov     ebx,    eax         ; ebx = inp_buff address (from caller)
         movzx   eax,    byte [ebx]  ; Load first digit (ASCII)
         sub     eax,    '0'         ; Convert to integer
-        cmp     eax,    4           ; Check for Visa (4)
-        je      .visa
-        cmp     eax,    6           ; Check for Discover (6)
-        je      .check_discover
-        cmp     eax,    5           ; Check for MasterCard (5)
-        je      .check_mastercard
-        cmp     eax,    3           ; Check for Amex (3)
-        je      .check_amex
-        jmp     .unknown            ; None matched
+        cmp     eax, 6
+        ja      .unknown            ; None matched
+        jmp     [jmp_tbl + eax * 4]
 
 .check_mastercard:                  ; Check second digit for MasterCard (51-55)
         movzx   eax,    byte [ebx + 1]  ; Load second digit
@@ -102,3 +105,4 @@ detect_card_type:                   ; Card type detection module
         pop     ebx                 ; Restore registers
         pop     eax
         ret                         ; Return to caller
+
